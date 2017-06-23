4e499cb963bef5954a4281fec89f69080bdf7cd5
Luis Van Slageren <luisvsm@gmail.com>
2017-06-23 22:43:29 +1000

    Stubbing out controllers

---
+++ Unity/LetsCurl/Assets/Game/Scripts/Controllers/NetworkController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController:MonoBehaviour {
+	public string host = "ws://localhost";
+	private WebSocket ws;
+	private IEnumerator wsCoroutine;
+
+	private static NetworkController _instance;
+	public static NetworkController Instance {
+		get{
+			if(_instance == null){
+				_instance = GameObject.Find("NetworkController").GetComponent<NetworkController>();
+				_instance.Init();
+			}
+			return _instance;
+		}
+	}
+
+	public void Init(){
+		ws = new WebSocket(new Uri(host));
+		wsCoroutine = SocketLoop();
+        StartCoroutine(wsCoroutine);
+	}
+
+	IEnumerator SocketLoop(){
+		yield return StartCoroutine(ws.Connect());
+		ws.SendString("Hi there");
+		int i=0;
+		while (true)
+		{
+			string reply = ws.RecvString();
+			if (reply != null)
+			{
+				Debug.Log ("Received: "+reply);
+				ws.SendString("Hi there"+i++);
+			}
+			if (ws.error != null)
+			{
+				Debug.LogError ("Error: "+ws.error);
+				break;
+			}
+			yield return 0;
+		}
+		ws.Close();
+	}
+}
