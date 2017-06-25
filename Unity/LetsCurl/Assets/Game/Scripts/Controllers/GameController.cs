36df1caa62235f45f2ef8f0474bb32cc13af0da2
Luis Van Slageren <luisvsm@gmail.com>
2017-06-25 13:14:32 +1000

    Added stone class

--- Unity/LetsCurl/Assets/Game/Scripts/Controllers/GameController.cs
+++ Unity/LetsCurl/Assets/Game/Scripts/Controllers/GameController.cs
@@ -10,25 +10,25 @@ public class GameController : BootableMonoBehaviour {
 	public GameObject distanceMeter;
 	public Text distanceMeterText;
 	public GameObject stonePrefab;
-	private GameObject _currentStone;
+	private Stone _currentStone;
 	public float originalFriction = 0.08f;
-	List<GameObject> StonePool = new List<GameObject>();
+	List<Stone> StonePool = new List<Stone>();
 	public Vector3 followCameraPosition = new Vector3(0f, 38.3f, -15f);
 	public Vector3 followRotation = new Vector3(56.3f, 0f, 0f);
 	public Vector3 restingCameraPosition =  new Vector3(0f, 20f, -71.1f);
 	public Vector3 restingRotation = new Vector3(42.7f, 0f, 0f);
-	GameObject CurrentStone{
+	Stone CurrentStone{
 		get{
 			if(_currentStone == null){
 				for (int i = 0; i < StonePool.Count; i++)
 				{
-					if(!StonePool[i].activeSelf){
+					if(!StonePool[i].gameObject.activeSelf){
 						_currentStone = StonePool[i];
-						_currentStone.SetActive(true);
+						_currentStone.gameObject.SetActive(true);
 						return _currentStone;
 					}
 				}
-				_currentStone = Instantiate(stonePrefab, Vector3.zero, Quaternion.identity);
+				_currentStone = Instantiate(stonePrefab, Vector3.zero, Quaternion.identity).GetComponent<Stone>();
 				StonePool.Add(_currentStone);
 			}
 			return _currentStone;
@@ -44,7 +44,7 @@ public class GameController : BootableMonoBehaviour {
 		if(Mathf.Abs(positionX - CurrentStone.transform.position.x) > 3 || CurrentStone.transform.position.y > positionY){
 			return;
 		}
-		CurrentStone.GetComponent<Rigidbody>().AddForce(new Vector3(1.5f * Time.deltaTime * (positionX - CurrentStone.transform.position.x), 0f, 0f), ForceMode.VelocityChange);
+		CurrentStone.body.AddForce(new Vector3(1.5f * Time.deltaTime * (positionX - CurrentStone.transform.position.x), 0f, 0f), ForceMode.VelocityChange);
 		floorMaterial.dynamicFriction = Mathf.Lerp(floorMaterial.dynamicFriction, 0f, 0.15f);
 	}
 	public void RestoreFriction(){
@@ -55,27 +55,27 @@ public class GameController : BootableMonoBehaviour {
 			position.z > -36 ||
 			(Camera.main.transform.position - restingCameraPosition).magnitude > 0.1f
 		){
-			CurrentStone.SetActive(false);
+			CurrentStone.gameObject.SetActive(false);
 			CurrentStone = null;
 			return;
 		}
-		CurrentStone.SetActive(true);
+		CurrentStone.gameObject.SetActive(true);
 		CurrentStone.transform.position = position;
 		CurrentStone.transform.eulerAngles = new Vector3(0f,0f,0f);
-		CurrentStone.GetComponent<Rigidbody>().AddForce(-CurrentStone.GetComponent<Rigidbody>().velocity, ForceMode.VelocityChange);
+		CurrentStone.GetComponent<Rigidbody>().AddForce(-CurrentStone.body.velocity, ForceMode.VelocityChange);
 	}
 	public void ThrowStone(Vector3 position, Vector3 forceVector){
 		if(
 			position.z > -36 ||
 			(Camera.main.transform.position - restingCameraPosition).magnitude > 0.1f
 		){
-			CurrentStone.SetActive(false);
+			CurrentStone.gameObject.SetActive(false);
 			CurrentStone = null;
 			return;
 		}
 		distanceMeter.SetActive(true);
-		CurrentStone.SetActive(true);
-		CurrentStone.GetComponent<Rigidbody>().AddForce(-CurrentStone.GetComponent<Rigidbody>().velocity, ForceMode.VelocityChange);
+		CurrentStone.gameObject.SetActive(true);
+		CurrentStone.GetComponent<Rigidbody>().AddForce(-CurrentStone.body.velocity, ForceMode.VelocityChange);
 		CurrentStone.transform.position = position;
 		CurrentStone.transform.eulerAngles = new Vector3(0f,0f,0f);
 		CurrentStone.GetComponent<Rigidbody>().AddForce((forceVector*forceMuliplyer), ForceMode.VelocityChange);
@@ -135,7 +135,7 @@ public class GameController : BootableMonoBehaviour {
 	private void resetShot(){
 		distanceMeter.SetActive(false);
 		CurrentStone = null;
-		CurrentStone.SetActive(false);
+		CurrentStone.gameObject.SetActive(false);
 		followStone = false;
 		InputController.Instance.throwing = true;
 		InputController.Instance.sweeping = false;
@@ -144,8 +144,8 @@ public class GameController : BootableMonoBehaviour {
 	public void CleanUpFallenStones(){
 		for (int i = 0; i < StonePool.Count; i++)
 		{
-			if(StonePool[i].activeSelf && StonePool[i].transform.position.y < -1){
-				StonePool[i].SetActive(false);
+			if(StonePool[i].gameObject.activeSelf && StonePool[i].transform.position.y < -1){
+				StonePool[i].gameObject.SetActive(false);
 				outOfBoundsAnimation.SetTrigger("FlashNow");
 			}
 		}
@@ -153,9 +153,9 @@ public class GameController : BootableMonoBehaviour {
 	public void CleanUpOutOfBounds(){
 		for (int i = 0; i < StonePool.Count; i++)
 		{	
-			if(StonePool[i].activeSelf){
+			if(StonePool[i].gameObject.activeSelf){
 				if(StonePool[i].transform.position.z > 69 || StonePool[i].transform.position.z < 36){
-					StonePool[i].SetActive(false); // Clean up rouge stones
+					StonePool[i].gameObject.SetActive(false); // Clean up rouge stones
 					outOfBoundsAnimation.SetTrigger("FlashNow");
 				}
 			}
@@ -164,7 +164,7 @@ public class GameController : BootableMonoBehaviour {
 	public bool StonesAreAtRest(){
 		for (int i = 0; i < StonePool.Count; i++)
 		{	
-			if(StonePool[i].activeSelf && StonePool[i].GetComponent<Rigidbody>().velocity.magnitude > 0.001){
+			if(StonePool[i].gameObject.activeSelf && StonePool[i].body.velocity.magnitude > 0.001){
 				return false;
 			}
 		}
---
+++ Unity/LetsCurl/Assets/Game/Scripts/Stone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour {
+
+	Rigidbody _rigidbody;
+	public Rigidbody body{
+		get{
+			if(_rigidbody == null)
+				_rigidbody = gameObject.GetComponent<Rigidbody>();
+			return _rigidbody;
+		}
+	}
+
+	enum Owner
+	{
+		Player1 = 1,
+		Player2 = 2,
+		Player3 = 3,
+		Player4 = 4
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
